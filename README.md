ccba7bf9f38c618c72be40b9827a79a07093bb38
Phil Ngo <ngo.phil@gmail.com>
2013-01-14 20:32:49 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+[Walk and Talk]
+This app originated as a ES21 project at harvard
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WalkNTalk::Application.config.secret_token = '72f768b3cea2049ab480bba295de299ed63384e102ba4329750124c2bfaacc3d2b9595b921d6fe04cdc73377b00236788a95593c5f3621d4f06a88efbccd9617'
